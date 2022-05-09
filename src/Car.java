02236ed3bd2ba91c10e310f56ff71020bee878d2
Sam Smith <romanmissilin@mail.ru>
2022-05-10 00:56:41 +0300

    Начало

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+
+public class Car extends Transport {
+
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void setPublishingWheelsCount(int wheelsCount) {
+        if (wheelsCount < 2 || wheelsCount > LocalDate.now().getYear()) {
+            System.out.println(wheelsCount + " некорректен, передайте корректный год!");
+            return;
+        }
+        this.wheelsCount = wheelsCount;
+    }
+    // А от этого есть смысл или я не правильно это реализовал?
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
